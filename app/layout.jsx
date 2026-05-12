36b450bd7a4833864726a2696d4d6693aadfd2e9
Akshansh Kakkar <akshanshkakkar92@gmail.com>
2026-05-12 23:53:45 +0530

    made the navbar responsive

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,54 +1,154 @@
-"use client"
+"use client";
 import Link from "next/link";
 import "./globals.css";
-import { Roboto, Poppins, Inter } from 'next/font/google'
+import { Roboto, Poppins, Inter } from "next/font/google";
 import { usePathname } from "next/navigation";
-
+import { motion } from "framer-motion";
+import { Cross, Menu, X } from "lucide-react";
+import { useState } from "react";
 const roboto = Roboto({
-  subsets : ["latin"]
-})
+  subsets: ["latin"],
+});
 const poppins = Poppins({
-  weight  : ["500", "700"],
-  subsets : ["latin"]
-})
+  weight: ["500", "700"],
+  subsets: ["latin"],
+});
 const inter = Inter({
-  weight : ["100", "700"],
-  subsets : ["latin"]
-})
+  weight: ["100", "700"],
+  subsets: ["latin"],
+});
 export default function RootLayout({ children }) {
   const pathname = usePathname();
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(!open);
+  };
   return (
     <html lang="en">
       <body className="min-h-full flex flex-col">
-        <nav>
+        <nav className="md:block hidden">
           <div className="flex justify-between items-center  px-18 py-8">
-            <div className={`text-2xl font-bold tracking-widest text-[#091426] ${roboto.className}`} ><Link href={'/'}>BlogModern</Link></div>
+            <div
+              className={`text-2xl font-bold tracking-widest text-[#091426] ${roboto.className}`}
+            >
+              <Link href={"/"}>BlogModern</Link>
+            </div>
             <div>
               <ul className={`flex gap-8 text-lg transition-all duration-300`}>
                 <li>
-                  <Link href={"/"} className={` transition-all duration-300 ${poppins.className} ${pathname==='/' ? "text-[#4B41E1] border-b-3 " : "text-gray-600 hover:text-black"} `}>Home</Link>
+                  <Link
+                    href={"/"}
+                    className={` transition-all duration-300 ${poppins.className} ${pathname === "/" ? "text-[#4B41E1] border-b-3 " : "text-gray-600 hover:text-black"} `}
+                  >
+                    Home
+                  </Link>
                 </li>
                 <li>
-                  <Link href={"/about"}className={` transition-all duration-300 ${poppins.className} ${pathname==='/about' ? "text-[#4B41E1] border-b-3 " : "text-gray-600 hover:text-black"} `}>About</Link>
+                  <Link
+                    href={"/about"}
+                    className={` transition-all duration-300 ${poppins.className} ${pathname === "/about" ? "text-[#4B41E1] border-b-3 " : "text-gray-600 hover:text-black"} `}
+                  >
+                    About
+                  </Link>
                 </li>
                 <li>
-                  <Link href={"/blog"} className={` transition-all duration-300 ${poppins.className} ${pathname==='/blog' ? "text-[#4B41E1] border-b-3 " : "text-gray-600 hover:text-black"} `}>Blog</Link>
+                  <Link
+                    href={"/blog"}
+                    className={` transition-all duration-300 ${poppins.className} ${pathname === "/blog" ? "text-[#4B41E1] border-b-3 " : "text-gray-600 hover:text-black"} `}
+                  >
+                    Blog
+                  </Link>
                 </li>
                 <li>
-                  <Link href={"/contact"} className={` transition-all duration-300 ${poppins.className} ${pathname==='/contact' ? "text-[#4B41E1] border-b-3 " : "text-gray-600 hover:text-black"} `}>Contact</Link>
+                  <Link
+                    href={"/contact"}
+                    className={` transition-all duration-300 ${poppins.className} ${pathname === "/contact" ? "text-[#4B41E1] border-b-3 " : "text-gray-600 hover:text-black"} `}
+                  >
+                    Contact
+                  </Link>
                 </li>
               </ul>
             </div>
-            <div>
-              <Link href={"/email"} className={`${inter.className} hover:bg-[#4c41e1df] font-bold p-3 rounded-lg bg-[#4B41E1] text-white`}>Get Stared</Link>
-            </div>
-           
+            <motion.div whileTap={{ scale: 1.05 }} whileHover={{ scale: 0.95 }}>
+              <Link
+                href={"/email"}
+                className={`${inter.className} hover:bg-[#4c41e1df] font-bold p-3 rounded-lg bg-[#4B41E1] text-white`}
+              >
+                Get Stared
+              </Link>
+            </motion.div>
           </div>
-           <div className="flex justify-center items-center">
-            <div className="bg-[#4B41E1] rounded-full mx-22 w-full h-[0.5px] ">
-
+          <div className="flex justify-center items-center">
+            <div
+              className="bg-[#4B41E1] rounded-full opacity-30
+0 mx-22 w-full h-[0.2px] "
+            ></div>
+          </div>
+        </nav>
+        <nav className="md:hidden block">
+          <div className="flex justify-between items-center px-8 py-4 sm:px-12 sm:py-8">
+            <div
+              className={`text-xl sm:text-2xl opaci4 font-bold tracking-widest text-[#091426] ${roboto.className}`}
+            >
+              <Link href={"/"}>BlogModern</Link>
             </div>
-           </div>
+            <div onClick={handleOpen} className="duration-300 transition-all">
+              {
+                open === true ? (<Menu/>):( <X />)
+              }            </div>
+          </div>
+          <div>
+            {open && (
+              <div className="flex justify-start ">
+                <div className="w-fit mx-6 flex justify-center flex-col items-center gap-6 p-12 rounded-xl my-2  bg-[#4c41e144]">
+                  <ul>
+                    <li>
+                      <Link
+                        className={`text-lg text-[#091426] ${poppins.className} ${pathname === "/" ? "underline font-medium" : ""}`}
+                        href={"/"}
+                      >
+                        Home
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        className={`text-lg text-[#091426] ${poppins.className} ${pathname === "/about" ? "underline font-medium" : ""}`}
+                        href={"/about"}
+                      >
+                        About
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        className={`text-lg text-[#091426] ${poppins.className} ${pathname === "/blog" ? "underline font-medium" : ""}`}
+                        href={"/blog"}
+                      >
+                        Blog
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        className={`text-lg text-[#091426] ${poppins.className} ${pathname === "/contact" ? "underline font-medium" : ""}`}
+                        href={"/contact"}
+                      >
+                        Contact
+                      </Link>
+                    </li>
+
+                  </ul>
+                                      <motion.div whileHover={{scale:1.1}} whileTap={{scale:0.95}}>
+                        <Link
+                          href={`/email`}
+                          className={`${inter.className} text:bg-[#4c41e1df] font-bold p-3 rounded-lg text-2xl-[#4B41E1] bg-white`}
+                        >
+                          Get Started
+                        </Link>
+                     
+                    </motion.div>
+                </div>
+              </div>
+            )}
+          </div>
         </nav>
         {children}
       </body>
