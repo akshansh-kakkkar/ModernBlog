3a943c2e5fe57b99db58839b129e0e67d953f9c5
Akshansh Kakkar <akshanshkakkar92@gmail.com>
2026-05-14 15:01:27 +0530

    added blog data files

---
+++ app/data/blog.js
@@ -0,0 +1,45 @@
+import { time } from "framer-motion";
+import { title } from "framer-motion/client";
+
+export const blog = [
+    {
+        id : 1,
+        title : " Getting started with react in 2026",
+        time : "5 min read",
+        description : "This blog includes starter guide about react click to read more about it.",
+        author : "John Doe",
+        image : "/images/react.png"
+    },
+    {
+        id : 2, 
+        title : "Why developers love NEXT JS",
+        time : "5 min read",
+        description : "This blog includes starter guide about NEXT JS click to read more about it.",
+        author : "Sarah",
+        image : "/images/next.png"
+    },
+    {
+        id : 3,
+        title : "Top 10 UI design trends",
+        time : "4 min read",
+        description : "This blog includes starter guide about Figma click to read more about it.",
+        author : "Akshansh",
+        image : "/images/figma.png"
+    },
+    {
+        id : 4,
+        title : "Mastering Javascript",
+        time : "7 min read",
+        description : "This blog includes starter guide about Javascript click to read more about it.",
+        author : "Michael",
+        image : "/images/javascript.png"
+    },
+    {
+        id : 5,
+        title : "Use of AI in Web development",
+        time : "8 min read",
+        description : "This blog includes started guide about how to use AI in web development click to read more about it.",
+        author : "David",
+        image : "/images/ai.png"
+    }
+]
